e5bb791f9c81d744b0f98b5bbf4383f84f3c938d
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-04-28 14:10:06 -0400

    added basic javascript to convert from celsius to fahrenheit if I pass in 25 degrees celsius as an argument to see if function is working

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// Function Declaration if degrees is 25 celsius
+// function celsiusToFahrenheit(celsius) {
+//   fahrenheit = (celsius / 5) * 9 + 32;
+//   return `25 degrees celsius is ${fahrenheit} degrees fahrenheit`;
+// }
+// console.log(celsiusToFahrenheit(25));
+
+// Arrow function if degrees is 25 celsius
+const celsiusToFahrenheit = (celsius) => (fahrenheit = (celsius / 5) * 9 + 32);
+console.log(celsiusToFahrenheit(25));
+
+// Need to grab number from input box and insert into function
