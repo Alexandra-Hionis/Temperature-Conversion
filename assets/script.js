f2c88eeef024c07cfac5fb70e02a8070d2d3b182
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-04-29 13:20:59 -0400

    added onclick to button in html. Created function to test whether or not when a value is entered into the celsius box, the button will alert the number entered. The alert is saying the same number I entered. Workign so far

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 // Function Declaration if degrees is 25 celsius
+
 // function celsiusToFahrenheit(celsius) {
 //   fahrenheit = (celsius / 5) * 9 + 32;
 //   return `25 degrees celsius is ${fahrenheit} degrees fahrenheit`;
@@ -6,7 +7,15 @@
 // console.log(celsiusToFahrenheit(25));
 
 // Arrow function if degrees is 25 celsius
-const celsiusToFahrenheit = (celsius) => (fahrenheit = (celsius / 5) * 9 + 32);
-console.log(celsiusToFahrenheit(25));
+// const celsiusToFahrenheit = (celsius) => (fahrenheit = (celsius / 5) * 9 + 32);
+// console.log(celsiusToFahrenheit(25));
 
 // Need to grab number from input box and insert into function
+
+// const fahrenheitToCelsius = (fahrenheit) => ()
+
+// .value is a property of text boxes where we can take the value inside the text box and store it in our str variable
+function fn1() {
+  var str = document.getElementById("text1").value;
+  alert("Value inside the text box is: " + str);
+}
